b122bfcc37b12bd2cd4c3bc80d6cf453d48ba1fc
Max Gecse <max.gecse@protonmail.com>
2026-02-14 15:24:36 +0100

    Update Anahera-alerting_system.c
    Licence has changed from MIT to Apache 2.0 license

--- Anahera-alerting_system.c
+++ Anahera-alerting_system.c
@@ -644,30 +644,7 @@ void Anahera_Task_20ms(const AnaheraObjectList_t* objs,
     }
 }
 
-/* ================== MIT License (as requested) ==================
 
-MIT License
-
-Copyright (c) 2026 Max Gecse
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights  
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell  
-copies of the Software, and to permit persons to whom the Software is  
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-/* NOTE: The following warning is explicitly requested by the author,
- * but conceptually conflicts with the permissive nature of the MIT License.
- * It is kept here verbatim per the author’s instructions.
- *
- * WARNING: Without explicit authorization from the author (Max Gecse),
- * this Software must not be used in production systems, and no code copying,
- * redistribution, or commercial use is allowed.
- */
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  
